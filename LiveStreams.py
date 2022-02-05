b07c6b84fad894507cf9b75bd5f200182acd8a5b
Thomas Joseph <trjosep@clemson.edu>
2022-02-05 01:03:38 -0500

    changes for merge

--- LiveStreams.py
+++ LiveStreams.py
@@ -2,10 +2,8 @@ from discord.ext import commands
 import discord
 
 class LiveStreams(commands.cog):
-    """Help for Twitch Cog"""
     def __init__(self,bot):
         self.bot = bot
-
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import discord
 import os
 from discord.ext import commands
@@ -6,12 +7,15 @@ import ErrorHandling
 import Reactions
 import Help
 
+with open('Bot_Secrets.json', 'r') as botSecrets:
+    settings = json.load(botSecrets)
+    TOKEN = settings['BotToken']
 
-TOKEN = ''
-GUILD = 'GUILD'
+intents = discord.Intents.default()
+intents.members = True
 
-intents = discord.Intents.all()
-client = commands.Bot(intents=intents, command_prefix='%')
+command_prefix = '%'
+client = commands.Bot(intents=intents, command_prefix=command_prefix)
 
 def addCog(client):
     ErrorHandling.setup(client)
@@ -24,7 +28,6 @@ async def on_ready(): # bot initialization
     discord_components.DiscordComponents(client)
     await client.change_presence(activity=discord.Game(name="Go Tigers! | %help"))
     print(f'{client.user} has connected to Discord!')
-    print(f'{GUILD}\n')
     client.remove_command('help') # gets rid of default help command on bot
 
     addCog(client)  # adding all related cogs to the bot
